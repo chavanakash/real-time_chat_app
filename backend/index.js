f69748a8248267dfb6237d369fb2c6ba021ca18b
sabavath akash <chavanakash2584@gmail.com>
2024-08-05 20:55:51 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+// const io = require('socket.io')(8000);
+
+// const users = {};
+
+// io.on('connection', socket => {
+//     socket.on('new-user-joined', name => {
+//         console.log("New user", name);
+//         users[socket.id] = name;
+//         socket.broadcast.emit('user-joined', name);
+//     });
+
+//     socket.on('send', message => {
+//         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+//     });
+
+//     socket.on('disconnect', message => {
+//         socket.broadcast.emit('left', users[socket.id]);
+//         delete users[socket.id];
+//     });
+// });
+
+//CHAT GPT code
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors'); // Import cors package
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*", // Allow all origins for simplicity, restrict as needed
+        methods: ["GET", "POST"]
+    }
+});
+
+// Use cors middleware
+app.use(cors());
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        console.log("New user", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+});
+
+server.listen(8000, () => {
+    console.log('Server is running on http://localhost:8000');
+});
+
