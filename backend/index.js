99c4d59cbbd5138342cb2a81c2cb1fbf7f5244c1
sabavath akash <87181111+chavanakash@users.noreply.github.com>
2024-08-05 21:05:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,4 @@
-// const io = require('socket.io')(8000);
 
-// const users = {};
-
-// io.on('connection', socket => {
-//     socket.on('new-user-joined', name => {
-//         console.log("New user", name);
-//         users[socket.id] = name;
-//         socket.broadcast.emit('user-joined', name);
-//     });
-
-//     socket.on('send', message => {
-//         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
-//     });
-
-//     socket.on('disconnect', message => {
-//         socket.broadcast.emit('left', users[socket.id]);
-//         delete users[socket.id];
-//     });
-// });
-
-//CHAT GPT code
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
