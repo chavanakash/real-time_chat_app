c445c53ae65ed07714e750ba66edac9e4a41366f
sabavath akash <87181111+chavanakash@users.noreply.github.com>
2025-06-28 20:53:35 +0530

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,4 @@
-const socket = io();
+const socket = io(window.location.origin);
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
