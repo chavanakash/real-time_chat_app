80c381527d2c32403164f18fa3e0d7dd49ff8ddc
sabavath akash <87181111+chavanakash@users.noreply.github.com>
2025-06-28 20:43:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -42,6 +42,7 @@ io.on('connection', socket => {
 });
 
 // Start server
-server.listen(8000, () => {
-    console.log('Server is running on http://localhost:8000');
+server.listen(8000, '0.0.0.0', () => {
+    console.log('Server is running on http://0.0.0.0:8000');
 });
+
